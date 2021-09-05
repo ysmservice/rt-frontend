03f18913db0b7c43b8125c9b8499fdf081ff204e
Takkun <takkun.053@gmail.com>
2021-09-05 16:16:20 +0900

    モジュールの動的読み込み

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import api_url from "./config.js"
 
 $(() => {
-    import js_file from location.pathname != "/dashboard.html" ? "./homepage.js" : "./dashboard.js"
-    js_file.main(api_url || location.origin, (new URL(location)).searchParams)
+    import(location.pathname != "/dashboard.html" ? "./homepage.js" : "./dashboard.js").then(module => {
+        module.main(api_url || location.origin, (new URL(location)).searchParams)
+    })
 })
