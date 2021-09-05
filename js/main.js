86fae203782c2912440475c98a778c0b19976012
Takkun <takkun.053@gmail.com>
2021-09-05 17:40:54 +0900

    これでいけるかなあ...()

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import api_url from "./config.js"
 
 $(() => {
-    import(location.pathname != "/dashboard.html" ? "./homepage.js" : "./dashboard.js").then(module => {
-        module.main(api_url || location.origin, (new URL(location)).searchParams)
-    })
+    import(location.pathname != "/dashboard.html" ? "./homepage.js" : "./dashboard.js")
+        .main(api_url || location.origin, (new URL(location)).searchParams)
 })
