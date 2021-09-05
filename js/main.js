18b1107cb707844f26cd299d8320e7ba43fbd420
Takkun <takkun.053@gmail.com>
2021-09-05 15:57:14 +0900

    import文が動くように

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import api_url from "./config"
+import api_url from "./config.js"
 
 $(() => {
-    import js_file from location.pathname != "/dashboard.html" ? "./homepage" : "./dashboard"
+    import js_file from location.pathname != "/dashboard.html" ? "./homepage.js" : "./dashboard.js"
     js_file.main(api_url || location.origin, (new URL(location)).searchParams)
 })
