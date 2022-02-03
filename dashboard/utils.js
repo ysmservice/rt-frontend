762908079f955aedfccf4585c0d9bc787a8f89b0
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-02-03 23:55:08 +0900

    update[text]: 説明とエラー文を変更

--- dashboard/utils.js
+++ dashboard/utils.js
@@ -2,7 +2,7 @@
 
 
 /**
- * ログインが必要と表示してからホーム画面にリダイレクトさせます。
+ * ログインが必要と表示してからログイン画面にリダイレクトさせます。
  */
 function requireLogin() {
     alert("ログインをしてください。");
@@ -19,10 +19,10 @@ export function checkResponse(response) {
     else if (response.status == 403)
         requireLogin();
     else if (response.status == 503) {
-        alert("りつちゃんは今お着替えか食事か通勤中です。(要するに準備中です。)\nもうちょっと待ってね。");
+        alert("りつちゃんは起きたばっかりで寝ぼけてます。(要するに準備中です。)\nもうちょっと待ってね。");
         window.location = "/dashboard";
     } else
-        alert("りつちゃん今風邪をひいているみたいです。\nすみませんが、今はここは使えません。");
+        alert("りつちゃんは今風邪をひいているみたいです。\nすみませんが、今はここは使えません。");
     return false;
 }
 
