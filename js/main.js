db0abb9d3263f751cf5b9fe0e58d3083209ac12e
tasuren <tasuren_0618@yahoo.co.jp>
2022-02-14 22:33:49 +0900

    coding: 多言語対応の作りかけ。

--- js/main.js
+++ js/main.js
@@ -5,3 +5,18 @@ $(() => {
         module.main(api_url || location.origin, (new URL(location)).searchParams)
     })
 })
+
+
+window.onload = function() {
+    // 日本語と英語の言語置き換えをする。
+    var lang = (navigator.language) ? navigator.language : navigator.userLanguage;
+    if (lang === undefined) lang = "ja";
+
+    for (let element of document.getElementsByClassName("language")) {    
+        if (lang.toLowerCase().indexOf("ja") === -1)
+            if (element.classList.contains("ja"))
+                element.hidden = false;
+        else if (element.classList.contains("en"))
+            element.hidden = false;
+    };
+}
